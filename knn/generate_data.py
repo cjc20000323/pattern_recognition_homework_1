b3210d98e4fe2d55d12b92c7cd777740c8b6458d
Lin-JL <1006480577@qq.com>
2023-03-15 20:50:00 +0800

    真实分布、训练集和测试集样本的可视化

--- knn/generate_data.py
+++ knn/generate_data.py
@@ -2,6 +2,9 @@ from scipy.stats import multivariate_normal
 import numpy as np
 from sklearn.model_selection import train_test_split
 import pickle
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['FangSong']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 """
 生成2元正态分布的随机数据，划分训练集和测试集，并保存数据到文件中
 """
@@ -31,5 +34,40 @@ if __name__ == '__main__':
     data_test = {'X': X_test, 'y': y_test}
 
     # 保存数据到文件
-    pickle.dump(data_train, open('data/data_train.pkl', 'wb'))
-    pickle.dump(data_test, open('data/data_test.pkl', 'wb'))
+    # pickle.dump(data_train, open('data/data_train.pkl', 'wb'))
+    # pickle.dump(data_test, open('data/data_test.pkl', 'wb'))
+
+    # 真实分布的可视化
+    XX, YY = np.mgrid[-4:4:0.1, -4:4:0.1]
+    Z0 = multivariate_normal.pdf(np.dstack([XX, YY]), mean=data0_mean, cov=data0_cov)
+    Z1 = multivariate_normal.pdf(np.dstack([XX, YY]), mean=data1_mean, cov=data1_cov)
+    # 真实分布的3D概率密度图
+    fig = plt.figure()
+    ax3 = plt.axes(projection='3d')
+    ax3.plot_surface(XX, YY, Z0, cmap='rainbow')
+    ax3.plot_surface(XX, YY, Z1, cmap='rainbow')
+    # plt.savefig('pictures/true_3D.png')
+    plt.show()
+    # 真实分布的等高线概率密度图
+    plt.contour(XX, YY, Z0)
+    plt.contour(XX, YY, Z1)
+    # plt.savefig('pictures/true_contour.png')
+    plt.show()
+
+    # 训练集样本可视化
+    X0_train = X_train[y_train == 0]
+    X1_train = X_train[y_train == 1]
+    plt.scatter(X0_train[:, 0], X0_train[:, 1], linewidths=0, s=10, alpha=0.5)
+    plt.scatter(X1_train[:, 0], X1_train[:, 1], linewidths=0, s=10, alpha=0.5)
+    plt.title('训练集样本')
+    # plt.savefig('pictures/scatter_train.png')
+    plt.show()
+
+    # 测试集样本可视化
+    X0_test = X_test[y_test == 0]
+    X1_test = X_test[y_test == 1]
+    plt.scatter(X0_test[:, 0], X0_test[:, 1], linewidths=0, s=10, alpha=0.5)
+    plt.scatter(X1_test[:, 0], X1_test[:, 1], linewidths=0, s=10, alpha=0.5)
+    plt.title('测试集样本')
+    # plt.savefig('pictures/scatter_test.png')
+    plt.show()
